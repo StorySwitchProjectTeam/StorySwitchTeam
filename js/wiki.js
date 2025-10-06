5184cb4916160997ed44b09c9c88900799f48937
Sopatrika <knydim@laposte.net>
2025-10-06 16:49:58 +0200

    new changes in menu

--- js/wiki.js
+++ js/wiki.js
@@ -1,16 +1,33 @@
 const listes = document.querySelectorAll(".menu_liste .element_liste");
 const sous_liste = document.querySelectorAll(".element_sous_liste");
 const li = document.querySelectorAll(".element_sous_liste li");
+const span = document.querySelectorAll(".element_liste span");
 
 listes.forEach(function(liste) {
     liste.addEventListener("click", function() {
         const sousListeId = this.id.replace("#", "") + "_sous";
         const sousListe = document.getElementById(sousListeId);
-        listes.forEach(el => el.classList.remove("element_liste_open"));
+        const currentSpan = this.querySelector("span");
+        const isAlreadyOpen = this.classList.contains("element_liste_open");
         
-        if (sousListe) {
-            sousListe.style.display = sousListe.style.display === "flex" ? "none" : "flex";
+        listes.forEach(el => {
+            el.classList.remove("element_liste_open");
+            const elSpan = el.querySelector("span");
+            if (elSpan) elSpan.classList.remove("open_span");
+        });
+        
+        sous_liste.forEach(sous => {
+            sous.style.display = "none";
+        });
+        
+        if (!isAlreadyOpen) {
+            this.classList.add("element_liste_open");
+            currentSpan.classList.add("open_span");
+            sousListe.style.display = "flex";
+        } else {
+            this.classList.remove("element_liste_open");
+            urrentSpan.classList.remove("open_span");
+            sousListe.style.display = "none";
         }
-        this.classList.toggle("element_liste_open");
     });
 });
