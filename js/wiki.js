78bd58e163fb677e228dbfc68902836229a44473
Sopatrika <knydim@laposte.net>
2025-10-24 15:55:26 +0200

    changes in wiki

--- js/wiki.js
+++ js/wiki.js
@@ -7,33 +7,17 @@ listes.forEach(function(liste) {
     liste.addEventListener("click", function() {
         const sousListeId = this.id.replace("#", "") + "_sous";
         const sousListe = document.getElementById(sousListeId);
-        const currentSpan = this.querySelector("span");
-        const isAlreadyOpen = this.classList.contains("element_liste_open");
+        const current_span = this.querySelector("span");
+        const Open = this.classList.contains("element_liste_open");
         
-        listes.forEach(el => {
-            el.classList.remove("element_liste_open");
-            const elSpan = el.querySelector("span");
-            if (elSpan) elSpan.classList.remove("open_span");
-        });
-        
-        sous_liste.forEach(sous => {
-            sous.style.display = "none";
-        });
-        
-        if (!isAlreadyOpen) {
-            this.classList.add("element_liste_open");
-            currentSpan.classList.add("open_span");
-            sousListe.style.display = "flex";
-        } else {
+        if (Open) {
             this.classList.remove("element_liste_open");
-            urrentSpan.classList.remove("open_span");
+            current_span.classList.remove("open_span");
             sousListe.style.display = "none";
+        } else {
+            this.classList.add("element_liste_open");
+            current_span.classList.add("open_span");
+            sousListe.style.display = "flex";
         }
     });
 });
-
-li.forEach(function(sous) {
-    li.addEventListener("click", function() {
-        
-    })
-})
