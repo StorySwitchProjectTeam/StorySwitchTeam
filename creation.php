53f413cce4d3c9e09d633234f58fb2f218b40df2
Argjend05 <argjendkallari@gmail.com>
2025-11-27 14:58:21 +0100

    Update creation.php

--- creation.php
+++ creation.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>StorySwitch Editor V11 (Server)</title>
+    <title>StorySwitch Editor V11.0.4</title>
     
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
